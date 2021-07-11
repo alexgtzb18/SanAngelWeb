66d635d6f01dc43a6c02dc10161e1699b7b245eb
alexgtzb18 <gbalex@live.com.mx>
2021-07-11 18:22:16 -0500

    Productos

--- src/Components/Navigator.js
+++ src/Components/Navigator.js
@@ -1,33 +1,35 @@
-import { Container, Header, Content, Footer, Sidebar, Navbar, Nav, Icon, Dropdown } from 'rsuite';
+import { Container, Header, Content, Footer, Navbar, Nav, Icon, Dropdown } from 'rsuite';
 
 const Navigator = () => {
     return (
-        <Container>
-            <Header>
-                <Navbar appearance="inverse">
-                    <Navbar.Header>
-                        <a className="navbar-brand logo">BRAND</a>
-                    </Navbar.Header>
-                    <Navbar.Body>
-                        <Nav>
-                            <Nav.Item icon={<Icon icon="home" />}>Home</Nav.Item>
-                            <Nav.Item>News</Nav.Item>
-                            <Nav.Item>Products</Nav.Item>
-                            <Dropdown title="About">
-                                <Dropdown.Item>Company</Dropdown.Item>
-                                <Dropdown.Item>Team</Dropdown.Item>
-                                <Dropdown.Item>Contact</Dropdown.Item>
-                            </Dropdown>
-                        </Nav>
-                        <Nav pullRight>
-                            <Nav.Item icon={<Icon icon="cog" />}>Settings</Nav.Item>
-                        </Nav>
-                    </Navbar.Body>
-                </Navbar>
-            </Header>
-            <Content>Content</Content>
-            <Footer>Footer</Footer>
-        </Container>
+        <div className="show-fake-browser navbar-page">
+            <Container>
+                <Header>
+                    <Navbar appearance="inverse">
+                        <Navbar.Header>
+                            <a className="navbar-brand logo">BRAND</a>
+                        </Navbar.Header>
+                        <Navbar.Body>
+                            <Nav>
+                                <Nav.Item icon={<Icon icon="home" />}>Home</Nav.Item>
+                                <Nav.Item>News</Nav.Item>
+                                <Nav.Item>Products</Nav.Item>
+                                <Dropdown title="About">
+                                    <Dropdown.Item>Company</Dropdown.Item>
+                                    <Dropdown.Item>Team</Dropdown.Item>
+                                    <Dropdown.Item>Contact</Dropdown.Item>
+                                </Dropdown>
+                            </Nav>
+                            <Nav pullRight>
+                                <Nav.Item icon={<Icon icon="cog" />}>Settings</Nav.Item>
+                            </Nav>
+                        </Navbar.Body>
+                    </Navbar>
+                </Header>
+                <Content>Content</Content>
+                <Footer>Footer</Footer>
+            </Container>
+        </div>
     )
 }
 export default Navigator
